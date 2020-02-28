a49d76d8fe2a52413b162b32a2c5d54aa85a81d1
Tim Snyder <snyder.tim@gmail.com>
2020-02-28 14:55:03 -0600

    handle bsd readlink on mac

--- install.sh
+++ install.sh
@@ -1,5 +1,31 @@
 #!/bin/sh
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+case "${machine}" in
+    Linux)
+	;;
+    Mac)
+	# use readlink from homebrew build of gnu coreutils
+	gnubin="/usr/local/opt/coreutils/libexec/gnubin"
+	if [[ ! -d "$gnubin" ]]; then
+	    echo "Error: can't find directory '$gnubin'"
+	    echo "       install https://formulae.brew.sh/formula/coreutils"
+	    echo "       before trying to setup your homedir"
+	    exit 1
+	fi
+
+	PATH="${gnubin}:$PATH"
+
+	;;
+esac
 
 
 # script is intended to be run in the directory where it lives
